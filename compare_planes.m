fe22f88936000e04c28c22320e8ca118ffa0b6d9
Matthew Poulter <mpoulter@google.com>
2017-09-14 12:07:22 -0700

    compare_planes.m   Working for single dataset and 1 cycle of 2 different calibration files. Next prepping for multiple runs of multiple cycles of two different calibration files within one dataset

--- compare_planes.m
+++ compare_planes.m
@@ -1,12 +1,14 @@
 %% Cleanup.
-clear all; clc; close all;
+clear; clc; close all;
 
 %% Params.
-% possible programs are {'vio', 'com'}
-desktop_program = 'vio';
+% possible programs are {'vio', 'com'}  Is overridden by CALIB_COMPARE_PROGRAM if exists.
+desktop_program = 'com';
+% set up data directory.  Is overridden by CALIB_COMPARE_DATASET if exists.
 data_base_dir = '/usr/local/google/home/mpoulter/for_matt/polaris_floor_vicon/mrinal/';
 data_sub_dir = '4';
 vicon_object_file = 'target_poses.txt';
+kViconGlobalOffset = .02;
 
 % TODO  Have calibration files parsed.
 % calib_file_1 = '';
@@ -19,6 +21,14 @@ i2_q_c2 = [-0.9998604612232053;  0.003304862190576937;  -0.007085454619255722;  
 
 %% Setup.
 data_path = fullfile(data_base_dir, data_sub_dir);
+exist CALIB_COMPARE_DATASET var;
+is_running_command_line = ans;
+exist CALIB_COMPARE_PROGRAM var;
+is_running_command_line = is_running_command_line && ans;
+if is_running_command_line
+    data_sub_dir = CALIB_COMPARE_DATASET;
+    desktop_program = CALIB_COMPARE_PROGRAM;
+end
 plane_folder_1 = fullfile(data_path, [desktop_program '_plane_1']);
 plane_folder_2 = fullfile(data_path, [desktop_program '_plane_2']);
 time_alignment_file = fullfile(data_path, 'time_alignment.txt');
@@ -51,69 +61,113 @@ targ_p_imu = ext(1,1:3)';
 targ_q_imu = ext(2,:)';
 
 %% Sync data.
-vicon_time = vicon_time/timing_data(1) + timing_data(2);
+vicon_time = vicon_time/timing_data(1) + timing_data(2);1
 
 tango_combined_timestamps = CombineTimestamps(tango_time_1, tango_time_2);
 cond = (tango_combined_timestamps <= max(vicon_time)) & ...
         (tango_combined_timestamps >= min(vicon_time)); 
 tango_combined_timestamps = tango_combined_timestamps(cond);
 
-[vg_p_targ_, vg_q_targ_] = interp_poses(vg_p_targ, vg_q_targ, ...
-                                 vicon_time', tango_combined_timestamps');
-
-%% Extract Vicon Global to Tango global from first time instance.
-tg_q_vg = quat_mul(tg_q_i_1(:,1), quat_inv(vg_q_i_(:,1)));
-tg_R_vg = quat2rot(tg_q_vg);
-tg_p_vg =  tg_p_i_1(:,1) - tg_R_vg * vg_p_i_(:,1);
-
-%% Convert to proper frames.
-% Convert to viconGlobal_T_imu
-N = length(vg_p_targ_);
-vg_p_i_ = zeros(3, N);
-vg_q_i_ = zeros(4, N);
+[vg_p_targ_, vg_q_targ_] = interp_poses(vg_p_targ', vg_q_targ', ...
+                                 vicon_time, tango_combined_timestamps);
+if isempty(vg_p_targ) 
+    disp('Empty set returned from interp_poses.   Exiting...');
+    return;
+end
+                             
+%%%%%%%%%%%%%%%%%%%%%   {FRAME} Conversions  %%%%%%%%%%%%%%%%%%%%                             
+%% Convert viconGlobal_T_target >>> viconGlobal_T_imu
+N = size(vg_p_targ_, 2);
+vg_p_i_ = zeros(N, 3);
+vg_q_i_ = zeros(N, 4);
 for i=1:N
-    vg_p_i_(:,i) = vg_p_targ_(:,i) + quat2rot(vg_q_targ_(:,i)) * targ_p_imu;
-    vg_q_i_(:,i) = quat_mul(vg_q_targ_(:,i), targ_q_imu);
+    vg_p_i_(i,:) = (vg_p_targ_(:,i) + quat2rot(vg_q_targ_(:,i)) * targ_p_imu)';
+    vg_q_i_(i,:) = quat_mul(vg_q_targ_(:,i), targ_q_imu)';
 end
 
-v_heights = vg_p_i_(3, :);
-t_heights_1 = [];
+
+%% Extract Vicon Global to Tango global from first time instance.
+tg_q_vg_1 = quat_mul(tg_q_i_1(1,:)', quat_inv(vg_q_i_(1,:)'));
+tg_R_vg_1 = quat2rot(tg_q_vg_1);
+tg_p_vg_1 =  tg_p_i_1(1,:)' - tg_R_vg_1 * vg_p_i_(1,:)';
+
+tg_q_vg_2 = quat_mul(tg_q_i_2(1,:)', quat_inv(vg_q_i_(1,:)'));
+tg_R_vg_2 = quat2rot(tg_q_vg_2);
+tg_p_vg_2 =  tg_p_i_2(1,:)' - tg_R_vg_2 * vg_p_i_(1,:)';
+
+%% Convert vicon height to Tango heights.
+% vicon_Floor2Imu = [zeros(size(vg_p_i_(:,2))) vg_p_i_(:, 2), zeros(size(vg_p_i_(:,2)))]; 
+vicon_floor2Imu_in_tangoFrame = (tg_R_vg_1 * vg_p_i_')'; vicon_floor2Imu_in_tangoFrame(:,3);
+
+%%
+tg_heights_1 = [];
+c_heights_1 = [];
 t_times_1 = [];
-t_heights_2 = [];
+tg_heights_2 = [];
+c_heights_2 = [];
 t_times_2 = [];
-for i = 1 : size(tango_combined_timestamps,2)
-   idxs_1 = find(plane_times_1 == tango_combined_timestamps(i));
-   idxs_2 = find(plane_times_2 == tango_combined_timestamps(i));
+for i = 1 : size(tango_combined_timestamps,1)
+   kTimeEpsilon = 1e-6;
+   height_idxs_1 = find(abs(plane_times_1(:,1) - tango_combined_timestamps(i)) < kTimeEpsilon);
+   height_idxs_2 = find(abs(plane_times_2(:,1) - tango_combined_timestamps(i)) < kTimeEpsilon);
+   tango_idx_1 = find(abs(tango_combined_timestamps(i) - tango_time_1 ) < kTimeEpsilon);
+   tango_idx_2 = find(abs(tango_combined_timestamps(i) - tango_time_2 ) < kTimeEpsilon);
    
-   h_1 = heights_1(idxs_1);
-   h_2 = heights_2(idxs_2);
+   h_1 = heights_1(height_idxs_1);
+   h_2 = heights_2(height_idxs_2);
    
    if ~isempty(h_1)
       % Sanity check.
       assert(all(h_1 == h_1(1))); 
-      % Save.
-      t_heights_1 = [t_height_1, h_1(1)];  
-      t_times_1 = [t_times_1, tango_combined_timestamps(i)];
+      assert (~isempty(tango_idx_1));
+      % Save height in {Tango_global}.
+      tg_heights_1 = [tg_heights_1; h_1(1)];  
+      t_times_1 = [t_times_1; tango_combined_timestamps(i)];
+      
+      % Save height in {IMU}
+      tg_p_i = tg_p_i_1(tango_idx_1, :);
+      c_heights_1 = [c_heights_1; -tg_p_vg_1(3) + tg_p_i(3)];
    end
    
    if ~isempty(h_2)
       % Sanity check.
       assert(all(h_2 == h_2(1)));
-      % Save.
-      t_heights_2 = [t_height_2, h_2(1)];
-      t_times_2 = [t_times_2, tango_combined_timestamps(i)];
+      assert (~isempty(tango_idx_2));
+      % Save height in {Tango_global}.
+      tg_heights_2 = [tg_heights_2; h_2(1)];
+      t_times_2 = [t_times_2; tango_combined_timestamps(i)];
+      
+      % Save heigth in {IMU}
+      tg_p_i = tg_p_i_1(tango_idx_1, :);
+      c_heights_2 = [c_heights_2; -tg_p_vg_2(3) + tg_p_i(3)];
    end   
+   disp(num2str(i));
 end
 
-%% Plot synchro-ed data.
-figure();
+%% Plot Floor heights in Tango Global.
+tango_fig = figure();
 hold on; grid on;
-plot(time_h, t_h1, '+b');
-plot(time_h, t_h2, 'xg');
-plot(time_h, v_heights, 'or');
-title('Floor height estimate')
+ylim([0 2]);
+plot(t_times_1, tg_heights_1, '+b');
+plot(t_times_2, tg_heights_2, 'og');
+plot(tango_combined_timestamps, repmat(-tg_p_vg_1(3) + kViconGlobalOffset, size(tango_combined_timestamps,1),1), '-r');
+title('Dominant Floor Plane Heights in Tango Global')
 legend([desktop_program ': 1'], [desktop_program ': 2'], 'Vicon');
-xlabel('time (s)'), ylabel('floor height (m)')
+xlabel('time (s)'); ylabel('floor height (m)');
+hold off;
+
+%% Plot Heights in IMU frame.
+imu_fig = figure();
+hold on; grid on;
+ylim([0 2]);
+plot(t_times_1, c_heights_1, '+b');
+plot(t_times_2, c_heights_2, 'og');
+plot(tango_combined_timestamps, vicon_floor2Imu_in_tangoFrame(:,3) + kViconGlobalOffset, '-r');
+title('Dominant Floor Plane Heights in Tango Global')
+legend([desktop_program ': 1'], [desktop_program ': 2'], 'Vicon');
+xlabel('time (s)'); ylabel('floor height (m)');
+
+%% Calculate RMSE.
 err_1 = abs(v_h-t_h1);
 rmse_fh1 = sqrt(mean(err_1(t_h1 > 1).^2));
 
@@ -125,15 +179,22 @@ disp(['Floor height RMSE_2 (m): ' num2str(rmse_fh2)])
 
 
 %% Helper Functions
-function [tg_q_i, tg_p_i, tango_time] = ExtractTangoData(tango_poses)
-    disp([tango_poses, 'ExtractTangoData not implemented']);
-    tg_q_i = [];
-    tg_p_i = [];
-    tango_time = [];
+function [tg_q_i, tg_p_i, tango_time] = ExtractTangoData(tango_poses_file)
+    tango_traj = dlmread(tango_poses_file);
+    tango_time = tango_traj(:,1);
+    if ~isempty(tango_time)
+       tg_p_i = tango_traj(:,2:4);
+       tg_q_i = tango_traj(:,5:8);
+    else 
+        tg_p_i = [];
+        tg_q_i = [];
+    end
 end
 
 function [heights, timestamps] = ExtractFloorHeights(directory)
-    kMinFloorMeasurements = 10;
+    heights = [];
+    timestamps = [];
+    kMinFloorMeasurements = 20;
     files = getAllFiles(directory);
     data = [];
     for i = 1 : size(files, 1)
@@ -169,12 +230,14 @@ end
 
 
 function [vg_q_targ, vg_p_targ, vicon_time] = ExtractViconData(vicon_file)
+    % Returned format:   [vector1'; vector2'; ...; vectorN]
+    vg_q_targ = []; vg_p_targ = []; vicon_time = [];
     data = dlmread(vicon_file);
     v_valid = sum(data(:,2:end),2) ~= 0;
     data = data(v_valid, :);
-    vicon_time = data(:,1)';
+    vicon_time = data(:,1);
     
-    vg_p_targ = data(:,2:4)'/1000;
+    vg_p_targ = data(:,2:4)/1000;
     targ_q_vg = data(:,5:8);
     diffs = sum(diff(targ_q_vg),2);
     sign_flag = 1;
@@ -185,16 +248,17 @@ function [vg_q_targ, vg_p_targ, vicon_time] = ExtractViconData(vicon_file)
         targ_q_vg(i+1,:) = targ_q_vg(i+1,:)*sign_flag;
     end
 
-    for i=1:length(vg_p_targ)
-        vg_q_targ(:,i) = quat_inv(targ_q_vg(i,:)');
+    for i=1:size(vg_p_targ, 1)
+        vg_q_targ(i,:) = quat_inv(targ_q_vg(i,:)')';
     end
 end
 
 function times = CombineTimestamps(tango_time_1, tango_time_2)
-    times = [tango_time_1, tango_time_2];
+    %  Union the set of timestamps and return in sorted order.
+    times = [tango_time_1', tango_time_2']';
     times = sort(times);
-    bools = logical(ones(1,size(times,2)));
-    for i = 2 : size(times,2)
+    bools = logical(ones(size(times,1), 1));
+    for i = 2 : length(times)
         if times(i) == times(i-1)
             bools(i) = false;
         end
